ffe69afcce8f0e686d5978e3d60b969e0ea32377
Serhii <serj1496@gmail.com>
2024-11-20 15:04:44 +0200

    lesson21

---
+++ script.js
@@ -0,0 +1,18 @@
+let firstNumber;
+let secondNumber;
+let result1, result2, result3, result4;
+
+firstNumber = prompt("Enter first number");
+secondNumber = prompt("Enter second number");
+
+result1 = +firstNumber + +secondNumber;
+alert(`Сума: ${result1}`);
+
+result2 = firstNumber - secondNumber;
+alert(`Різниця: ${result2}`);
+
+result3 = firstNumber * secondNumber;
+alert(`Множення: ${result3}`);
+
+result4 = firstNumber / secondNumber;
+alert(`Ділення: ${result4}`);
