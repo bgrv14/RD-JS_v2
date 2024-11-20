95fac99fb3a74828cebfcfa81dc74fd1a360bd64
Serhii <serj1496@gmail.com>
2024-11-20 20:40:34 +0200

    lwsson22

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 let firstNumber;
 let secondNumber;
-let result1, result2, result3, result4;
+let result1;
+let result2;
+let result3;
+let result4;
 
 firstNumber = prompt("Enter first number");
 secondNumber = prompt("Enter second number");
 
-result1 = +firstNumber + +secondNumber;
-result2 = firstNumber - secondNumber;
-result3 = firstNumber * secondNumber;
-result4 = firstNumber / secondNumber;
-alert(`Сума: ${result1};
-Різниця: ${result2}
-Множення: ${result3}
-Ділення: ${result4}`);
+if (!firstNumber || !secondNumber) {
+  alert("Помилка");
+} else {
+  result1 = +firstNumber + +secondNumber;
+  alert(`Сума: ${result1}`);
+
+  if (secondNumber < firstNumber == false) {
+    if (confirm("Ви впевнені, що хочете продовжити операцію?") === true) {
+      result2 = +firstNumber - +secondNumber;
+      alert(`Різниця: ${result2}`);
+    } else {
+    }
+  } else {
+    result2 = +firstNumber - +secondNumber;
+    alert(`Різниця: ${result2}`);
+  }
+
+  result3 = +firstNumber * +secondNumber;
+  alert(`Множення: ${result3}`);
+
+  if (secondNumber == 0) {
+    alert("Помилка: Друге число не може дорівнювати нулю!");
+  } else {
+    result4 = +firstNumber / +secondNumber;
+    alert(`Ділення: ${result4}`);
+  }
+}
