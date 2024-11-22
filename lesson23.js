05236540ebe37b80c89cdade959aa2f47b4ca522
Serhii <serj1496@gmail.com>
2024-11-22 17:15:29 +0200

    lesson24

--- lesson23.js
+++ lesson23.js
@@ -1,11 +1,17 @@
 const arr = [4, 8, 15, 16, 23, 42, 7, 14, null, 96];
 
 let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] === "number") {
+    sum += arr[i];
+  }
+}
+console.log(sum);
+
 let min = Infinity;
 let max = -Infinity;
 for (let i = 0; i < arr.length; i++) {
   if (typeof arr[i] === "number") {
-    sum += arr[i];
     if (arr[i] < min) {
       min = arr[i];
     }
@@ -14,7 +20,7 @@ for (let i = 0; i < arr.length; i++) {
     }
   }
 }
-console.log(sum);
+
 console.log("Мін:", min, "Макс:", max);
 
 let str = "";
@@ -22,3 +28,59 @@ for (let i = 1; i <= 5; i++) {
   str += "#".repeat(i) + "\n";
 }
 console.log(str);
+
+function checknumber(a, b) {
+  return typeof a === "number" && typeof b === "number";
+}
+
+function add(a, b) {
+  if (checknumber(a, b)) {
+    return a + b;
+  } else {
+    return "Помилка: введіть числа";
+  }
+}
+
+function subtract(a, b) {
+  if (checknumber(a, b)) {
+    return a - b;
+  } else {
+    return "Помилка: введіть числа";
+  }
+}
+
+function multiply(a, b) {
+  if (checknumber(a, b)) {
+    return a * b;
+  } else {
+    return "Помилка: введіть числа";
+  }
+}
+
+function divide(a, b) {
+  if (checknumber(a, b) && b !== 0) {
+    return a / b;
+  } else {
+    return "Помилка: введіть числа та переконайтеся, що друге число не є нулем";
+  }
+}
+
+function findMin(arr) {
+  let min = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number" && arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+function findMax(arr) {
+  let max = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number" && arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
