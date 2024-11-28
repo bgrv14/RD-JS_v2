f563e1ba02933d8e47649a47f87555d5f952e8e0
Serhii <serj1496@gmail.com>
2024-11-28 20:11:39 +0200

    lesson29

---
+++ lesson29.js
@@ -0,0 +1,39 @@
+// 1
+function calculateAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  let avg = sum / arr.length;
+  return avg;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+
+let avg = calculateAverage(numbers);
+console.log("The average is: " + avg);
+
+// 2
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+User.prototype.sayHello = function () {
+  return `Hi. My name is ${this.name} and I'm ${this.age} years old`; // додано this до виклику name та виправлено помилку в форматуванні this.age
+};
+
+const user = new User("Sherlock", 27);
+console.log(user.sayHello());
+
+// 3
+const calculator = function (init = 0) {
+  return function () {
+    return ++init;
+  };
+};
+
+const c = calculator();
+console.log(c());
+console.log(c());
